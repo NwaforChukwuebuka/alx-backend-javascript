f1e53b5bdc4130cb6a583ff4b940b1df80f55fc5
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-08-03 03:16:42 +0000

    Lint checked

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,9 +2,9 @@ const getListStudents = () => {
   const studentList = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return studentList;
-}
+};
 
 export default getListStudents;
